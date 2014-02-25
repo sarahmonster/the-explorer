d0379a180c98c52a20a0ed1f96f2f8f34c05a943
sarah <sarah@triggersandsparks.com>
2014-02-25 00:29:52 -0500

    It is really hard watching matt use puter Because he is dumb

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-$(document).foundation();
+$(function() {
+	$(document).foundation();
+
+	var didFadeIn = localStorage ? localStorage['loaded'] : false;
+
+	$('.magellan-container').css({
+		position: 'fixed',
+		top: 0
+	});
+
+	if (didFadeIn) {
+		$('body').addClass('js loaded');
+	} else {
+		$('body').addClass('js');
+		
+		$(window).on('load', function() {
+	   		$('body').addClass('loaded');
+	   		localStorage['loaded'] = '1';
+	    });
+	}
+});
