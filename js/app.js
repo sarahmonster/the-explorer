c235f98b5088da9189df8421f3ccdafa4d4fd54e
sarah <sarah@triggersandsparks.com>
2014-03-07 20:36:47 -0500

    Remove js fade-in effect.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,9 @@
 $(function() {
 	$(document).foundation();
 
-	var didFadeIn = localStorage ? localStorage['loaded'] : false;
-
 	$('.magellan-container').css({
 		position: 'fixed',
 		top: 0
 	});
 
-	if (didFadeIn) {
-		$('body').addClass('js loaded');
-	} else {
-		$('body').addClass('js');
-		
-		$(window).on('load', function() {
-	   		$('body').addClass('loaded');
-	   		localStorage['loaded'] = '1';
-	    });
-	}
 });
